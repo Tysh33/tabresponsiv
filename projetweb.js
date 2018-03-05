8f778363caf7d78abb1976b862e7c26886089d1e
Tysh33 <37068224+Tysh33@users.noreply.github.com>
2018-03-05 11:01:07 +0100

    Add files via upload

---
+++ projetweb.js
@@ -0,0 +1,63 @@
+new Vue ({
+    el : "#root",
+    data : {
+        titre: "",
+        resume: "",
+        affect:"",
+        client: "",
+        displayModal : false,
+        gridColumns : ["","ID","Titre","Résumé","Affecté à","Client","Etat"],
+        gridDatas: [
+            { ID: 'Chuck Norris', Titre: Infinity, Resume: "Americain", Affect: "Paul", Client:"Toto", Etat: "En cours" },
+            { ID: 'Bruce Lee', Titre: 9000, Resume: "Chinois", Affect: "Ken", Client:"Tata", Etat: "En cours"},
+            { ID: 'Jackie Chan', Titre: 7000, Resume: "Hongkongais", Affect: "Boris", Client:"Titi", Etat: "En cours"},
+            { ID: 'Jet Li', Titre: 8000, Resume: "Chinois", Affect: "Romain", Client:"Tutu", Etat: "En cours"}],
+    },
+    methods: {
+
+        toggleModal() {
+
+            this.displayModal = !this.displayModal
+            this.$refs.createform.reset()
+        },
+
+        getForm() {
+            let item = {
+                ID: this.gridDatas.length + 1,
+                Titre: this.titre,
+                Resume: this.resume,
+                Affect: this.affect,
+                Client: this.client,
+                Etat: "En cours",
+            }
+
+            this.gridDatas.push(item)
+            this.displayModal = false
+            this.$refs.createform.reset()
+        },
+
+
+        deleteForm() {
+            console.log(this.gridDatas.length)
+            for (i = this.gridDatas.length-1; i > 0; --i) {
+                console.log("jkhkj")
+                if (this.check === true)
+                this.gridDatas.remove()
+            }
+        }
+    }
+})
+
+let corps = document.getElementById("corps");
+function test() {
+    arrayColumns = document.getElementById("corps").rows
+    for (i=arrayColumns.length; i >0; --i) {
+        let j = i-1
+        let checkState = document.getElementById("corps").rows[j].cells[0].childNodes[0].checked
+        if (checkState == true) {
+            corps.removeChild(corps.rows[j])
+        }
+    }
+}
+
+
